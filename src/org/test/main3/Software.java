e95f8bdb0c94bf1bf118aa210efedb811aa9809c
jeyachitrachithu <jeyasa5191@gmail.com>
2019-09-19 12:37:21 +0530

    Learning my code

---
+++ src/org/test/main3/Software.java
@@ -0,0 +1,19 @@
+package org.test.main3;
+
+public class Software {
+	private void test6() {
+ System.out.println("english");
+ }
+	public void test4() {
+		System.out.println("tamil");
+		
+	}
+	public static void main(String[] args) {
+		Software r = new Software ();
+		r.test4();
+		r.test6();
+	}
+
+	}
+
+
